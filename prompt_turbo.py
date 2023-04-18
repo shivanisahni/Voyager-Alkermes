90313506f7d4a8e2c005a740cb6daeadbd70c4e7
Vedant Kumud <vedant.kumud@gmail.com>
2023-04-17 18:52:56 -0700

    cleaned up code, modified turbo

--- prompt_turbo.py
+++ prompt_turbo.py
@@ -4,41 +4,46 @@ import pandas as pd
 import numpy as np
 import time
 
-questions = []
-example_answers = {}
+def generate_examples(example_file = 'MoA_one_shot.csv'):
+    # TODO test if adding drug is better (need to pad examples then...!)
+    examples = pd.read_csv(example_file)
+    example_answers = {}
+    example_prompts = {}
+    
+    for index, row in examples.iterrows():
+        MoA = row['MoA']
+        example_answers[MoA] = row['Answers From Example Articles']
+        # Pass these in as arrays
+        MoA_titles = row['Titles From Example Articles']
+        MoA_abstracts = row['Abstracts From Example Articles']
 
-# Wording of the questions is important
-# questions.append("From the above article, what is the disease, indication, or medical condition being tested?")
-# questions.append("What is the drug that is being evaluated for on its efficacy or safety in the article?") # might hvae to edit toi get short or long name
-questions.append("What is the mechanism of action (target of the chemical reaction) and the receptor subtype that the drug is being evaluated with?")
-# questions.append("Was the clinical trial in the article evaluated in humans?")
-# questions.append("The phase (either 1, 2, or 3) that the clinical trial in the article was in is?")
-# questions.append("Did the drug from the article result in improvements in patients and was it successful in its job?")
-# questions.append("When was this clinical trial done (START and END date needed)?")
+        MoA_prompts = []
+        for i in range(len(MoA_titles)):
+            title = MoA_titles[i]
+            abstract = MoA_abstracts[i]
+            prompt = "Title: {}\n Abstract: {}".format(title, abstract)
+            MoA_prompts.append(prompt)
+        print(MoA_prompts)
+        
+        # Remove this once everything has examples
+        if MoA == "Partial Agonist":
+            break
+        
+        example_prompts[MoA] = MoA_prompts
 
-# TODO test if adding drug is better
-example_answers[questions[0]] = ["The mechanism of action is: antagonist. The receptor subtype is: D1 receptor antagonist.",
-        "The mechanism of action is: agonist/antagonist. The receptor subtype(s) are: selective estrogen receptor modulator agonist/antagonist",
-        "The mechanism of action is: antagonist. The receptor subtype(s) are: H-2 antagonist.",
-        "The mechanism of action is: agonist. The receptor subtype(s) are: dopamine D3-preferring D3/D2 receptor partial agonist and serotonin 5-HT1A receptor partial agonist."]
+    return example_prompts, example_answers
 
-
-########### MAKE SURE TO COMMENT THIS OUT WHEN COMMITTING ############
-openai.api_key = "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" # vedant key
-
-answers = []
-df = pd.DataFrame()
-
-def generate_prompt(article):
+def generate_prompt(article, MoA, example_prompts, example_answers):
     def generate_example(index):
         prompt = "I will give you an article then ask a few questions about it. \n"
-        prompt += examples[index] + "\n"
+        prompt += example_prompts[MoA][index] + "\n"
         prompt += "Here are the questions about the article: \n"
         idx = 0
         for question in questions:
             idx += 1
             prompt += "Q{}: ".format(idx)  + question + "\n"
-            prompt += "A{0}: {1}".format(idx, example_answers[question][index]) +"\n"
+            # if we care about drug too need to start indexing by question type
+            prompt += "A{0}: {1}".format(idx, example_answers[MoA][index]) +"\n"
         
         return prompt
 
@@ -54,7 +59,7 @@ def generate_prompt(article):
         
         return prompt
     total_prompt = ""
-    for i in range(4):
+    for i in range(len(example_prompts[MoA])):
         total_prompt += generate_example(i) + "\n\n"
     total_prompt += generate_actual()
     return total_prompt
@@ -74,40 +79,53 @@ def run_prompt(prompt):
     print(text)
     return text
 
-# TODO add titles on examples
-examples = []
-examples.append("""Studies in nonhuman primates documented that appropriate stimulation of dopamine (DA) D1 receptors in the dorsolateral prefrontal cortex (DLPFC) is critical for working memory processing. The defective ability of patients with schizophrenia at working memory tasks is a core feature of this illness. It has been postulated that this impairment relates to a deficiency in mesocortical DA function. In this study, D1 receptor availability was measured with positron emission tomography and the selective D1 receptor antagonist [11C]NNC 112 in 16 patients with schizophrenia (seven drug-naive and nine drug-free patients) and 16 matched healthy controls.""")
-examples.append("""A substantial proportion of women with schizophrenia experience debilitating treatment-refractory symptoms. The efficacy of estrogen in modulating brain function in schizophrenia has to be balanced against excess exposure of peripheral tissue. Raloxifene hydrochloride is a selective estrogen receptor modulator (mixed estrogen agonist/antagonist) with potential psychoprotective effects and fewer estrogenic adverse effects.
-To determine whether adjunctive raloxifene therapy reduces illness severity in women with refractory schizophrenia.""")
-examples.append("""Smooth pursuit eye movements (SPEM) are often abnormal in schizophrenic patients and have been proposed as a trait marker of the disorder. We explored the use of SPEM as an outcome measure in an open-label clinical trial of famotidine, an H-2 antagonist, in patients with schizophrenia; famotidine has been proposed as an adjunctive medication, particularly for negative symptoms.""")
-examples.append("""Negative symptoms in schizophrenia are heterogeneous and multidimensional; effective treatments are lacking. Cariprazine, a dopamine D3-preferring D3/D2 receptor partial agonist and serotonin 5-HT1A receptor partial agonist, was significantly more effective than risperidone in treating negative symptoms in a prospectively designed trial in patients with schizophrenia and persistent, predominant negative symptoms.""")
+def parse_all(file_name, example_prompts, example_answers):
+      answer_df = pd.DataFrame()
+      data_df = pd.read_csv(file_name)
+      ids = []
+      answers = []
+      num_processed = 0
 
-# TODO see if abstracts + title in csv are fully correct
-def parse_all(file_name = 'schiz_abstracts.csv'):
-      with open(file_name, 'r') as csv_file:
-        reader = csv.reader(csv_file)
+      for index, row in data_df.iterrows():
+          num_processed += 1
 
-        ids = []
-        idx = 0
-        for row in reader:
-            idx += 1
-            # See if merging of title and article is correct
-            curr_article = row[7]
-            curr_title = row[2]
-            prompt = generate_prompt("Title: {}\n Abstract: {}".format(curr_title, curr_article))
-            print("Curr ID answer {}:".format(row[5]))
-            ids.append(row[5])
-            ans = run_prompt(prompt)
-            answers.append(ans)
-            if idx >= 100:
-                break
-            time.sleep(1.1)
-        
-        print(len(answers))
-        print(len(ids))
-        df['Pub Med ID'] = ids
-        df['Answers'] = answers
-        df.to_csv('sampleresults_turbo.csv', index = False)
-        print(df)
+          title = row['ARTICLE TITLE']
+          abstract = row['ABSTRACT']
+          prompt = "Title: {}\n Abstract: {}".format(title, abstract)
+          MoA = row['MoA']
 
-parse_all()
+          prompt = generate_prompt(prompt, MoA, example_prompts, example_answers)
+          print("Curr ID answer {}:".format(row['PMID']))
+
+          ids.append(row['PMID'])
+          ans = run_prompt(prompt)
+          answers.append(ans)
+
+          if num_processed >= 100:
+              break
+          # time.sleep()
+
+      print(len(answers))
+      print(len(ids))
+      answer_df['Pub Med ID'] = ids
+      answer_df['Answers'] = answers
+      answer_df.to_csv('sampleresults_turbo.csv', index = False)
+      print(answer_df)
+
+if __name__ == "__main__":
+    # TODO add argument parser for file names
+    ########### MAKE SURE TO COMMENT THIS OUT WHEN COMMITTING ############
+    openai.api_key = "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" # vedant key
+
+    questions = []
+    questions.append("What are the mechanism of action(s) (target of the chemical reaction) and the receptor subtype(s) that the drug(s) are being evaluated with?")
+    # need to use acutal example csv to get examples
+    # example_prompts, example_answers = generate_examples()
+    example_prompts = {}
+    example_answers = {}
+    example_prompts['Partial Agonist'] =["""The Taq1A polymorphism in the dopamine D2 receptor (DRD2) gene could be related to the response to antipsychotics. We examined the effects of the Taq1A polymorphism on the plasma monoamine metabolites during the treatment of schizophrenia with aripiprazole, a DRD2 partial agonist.""", """This phase III study evaluated the efficacy and safety of cariprazine, a dopamine D3 and D2 receptor partial agonist with preferential binding to D3 receptors, in patients with acute exacerbation of schizophrenia. Patients were randomized to 6-week double-blind treatment with placebo, cariprazine 3 to 6 mg/d, or cariprazine 6 to 9 mg/d. """, """This study evaluated whether alterations in serotonin function in schizophrenic patients could be demonstrated by comparing the reactivity to a serotonin partial agonist, m-chlorophenylpiperazine (MCPP) in patients and healthy subjects. This study also assessed whether stimulation of serotonin receptors influenced the symptoms of schizophrenia."""]
+    example_answers['Partial Agonist'] = ["The mechanism of action is: partial agonist. The receptor subtype(s) are: dopamine D2 receptor partial agonist.", "The mechanism of action is: partial agonist. The receptor subtype(s) are: dopamine D3 and D2 receptor partial agonist.", "The mechanism of action is: partial agonist. The receptor subtype(s) are: serotonin partial agonist."]
+    print(example_prompts, example_answers)
+    parse_all('schiz_abstracts.csv', example_prompts,
+            example_answers)
+    
